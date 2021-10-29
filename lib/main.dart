06629b123c8817571e230828c610a633f8d15ccb
SolankiDaxesh <iet06solankidaxesh@gmail.com>
2021-10-29 14:38:59 +0530

    button

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void ans() {
+    print('Button pressed');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var ques = ['What\'s Your favorite car?', 'What\'s your favorite colour?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My App'),
         ),
-        body: Text('This is my app...'),
+        body: Column(
+          children: [
+            Text('Questions'),
+            ElevatedButton(child: Text('Answer 1'), onPressed: ans),
+            ElevatedButton(child: Text('Answer 2'), onPressed: ans),
+            ElevatedButton(child: Text('Answer 3'), onPressed: ans),
+          ],
+        ),
       ),
     );
   }
